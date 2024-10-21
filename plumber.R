6b0e41ad0dce46cd86736118edd04ae8e79d0e4a
João Victor Siqueira Rodrigues <jvsiqueira2013@gmail.com>
2024-10-21 15:54:32 -0300

    Modificacoes 21/10

--- plumber.R
+++ plumber.R
@@ -4,21 +4,23 @@ library(ggplot2)
 library(dplyr)
 library(farver)
 library(jsonlite)
+library(ggfocus)
 
 #* @apiTitle API Regressão Linear
+
 # Variável global
-# ra <- 185416
-# set.seed(ra)
-# b0 <- runif(1, -2, 2); b1 <- runif(1, -2, 2)
-# bB <- 2; bC <- 3
-# n <- 25
-# x <- rpois(n, lambda = 4) + runif(n, -3, 3)
-# grupo <- sample(LETTERS[1:3], size = n, replace = TRUE)
-# y <- rnorm(n, mean = b0 + b1*x + bB*(grupo=="B") + bC*(grupo=="C"), sd = 2)
-# df <- data.frame(x = x, grupo = grupo, y = y, momento_registro = lubridate::now(), 
-#                  ID = seq(1, length(x)))
-# readr::write_csv(df, file = "dados_regressao.csv")
-df <- read.csv("dados_regressao.csv")
+ra <- 185416
+set.seed(ra)
+b0 <- runif(1, -2, 2); b1 <- runif(1, -2, 2)
+bB <- 2; bC <- 3
+n <- 25
+x <- rpois(n, lambda = 4) + runif(n, -3, 3)
+grupo <- sample(LETTERS[1:3], size = n, replace = TRUE)
+y <- rnorm(n, mean = b0 + b1*x + bB*(grupo=="B") + bC*(grupo=="C"), sd = 2)
+df <- data.frame(x = x, grupo = grupo, y = y, momento_registro = lubridate::now(),
+                 ID = seq(1, length(x)))
+readr::write_csv(df, file = "dados/dados_regressao.csv")
+df <- read.csv("dados/dados_regressao.csv")
 modelo <- lm(y ~ x + as.factor(grupo), data = df)
 
 
@@ -27,22 +29,31 @@ modelo <- lm(y ~ x + as.factor(grupo), data = df)
 #* @param x Variável numérica
 #* @param grupo Variável categórica
 #* @param y Variável resposta
-#* @post /add_row
+#* @post /data/add_row
 function(x, grupo, y) {
   nova_pessoa <- data.frame(x = as.numeric(x), grupo = grupo, y = as.numeric(y),
                             momento_registro = lubridate::now(), ID = max(df$ID) + 1)
-  readr::write_csv(nova_pessoa, "dados_regressao.csv", append = TRUE)
+  readr::write_csv(nova_pessoa, "dados/dados_regressao.csv", append = TRUE)
   df <<- rbind(df, nova_pessoa)
 }
 
 # Parte 2 - Eletiva
 #* Remove observações pelo ID
 #* @param ID Identificador da linha
-#* @delete /delete_row
+#* @delete /data/delete_row
 function(ID) {
-  df <<- df[-as.numeric(ID),]
-  readr::write_csv(df, "dados_regressao.csv")
+  if (grepl(':', ID)) {
+    ID <- as.numeric(unlist(strsplit(ID,":")))
+    ID <- seq(ID[1], ID[2])
+  }
+  else {
+    ID <- as.numeric(unlist(strsplit(ID, ",")))
+  }
+  df <<- df[!(df$ID %in% ID),]
+  readr::write_csv(df, "dados/dados_regressao.csv")
 }
+# Remove sequências do tipo 1,2,3 quanto vetores em R 1:3.
+
 
 # Parte 2 - Eletiva
 #* Modifica uma observação pelo ID
@@ -50,31 +61,39 @@ function(ID) {
 #* @param x Variável numérica
 #* @param grupo Variável categórica
 #* @param y Variável resposta
-#* @put /change_row
+#* @put /data/change_row
 function(ID, x, y, grupo) {
   df[as.numeric(ID), ] <<- data.frame(x = as.numeric(x), grupo = grupo, 
   y = as.numeric(y), momento_registro = lubridate::now(), ID = as.numeric(ID))
-  readr::write_csv(df, "dados_regressao.csv")
+  readr::write_csv(df, "dados/dados_regressao.csv")
 }
+
 
 # Parte 3
 #* Gera um gráfico de dispersão com a reta de regressão ajustada por categoria
 #* @serializer png
-#* @get /plot_lm
-function() {
-  grafico <- df %>% ggplot(aes(x = x, y = y, col = grupo)) +
+#* @get /plot/lm
+function(focus = NULL) {
+  grafico <- df %>% ggplot(aes(x = x, y = y, col = grupo, alpha = grupo, group = grupo)) +
     geom_point() +
     geom_smooth(method = "lm", se = FALSE) +
     theme_bw() +
-    labs(title = "Gráfico de dispersão com a reta de regressão ajustada por categoria", 
-         subtitle = "Dados simulados", x = colnames(df)[1], y = colnames(df)[3])
-  print(grafico) 
+    labs(title = "Gráfico de dispersão com a reta de regressão ajustada por categoria",
+         x = colnames(df)[1], y = colnames(df)[3])
+  if (!(is.null(focus))) {
+    focus <- unlist(strsplit(focus, ","))
+    grafico <- grafico +
+      scale_alpha_focus(focus) +
+      scale_color_focus(focus)
+  }
+  print(grafico)
 }
+
 
 # Parte 3
 #* Fornece as estimativas dos betas e da variância
 #* @serializer json
-#* @get /stats
+#* @get /fit/param
 function() {
   modelo <- lm(y ~ x + as.factor(grupo), data = df)
   resultado <- list(
@@ -84,36 +103,38 @@ function() {
     beta3 = modelo$coefficients[4],          
     QME = anova(modelo)[3, 3]                
   )
-  return(toJSON(resultado, pretty = TRUE))
+  return(resultado)
 }
+
 
 # Parte 3 - Eletiva
 #* Retorna todos os resíduos do modelo de regressão ajustado
 #* @serializer json
-#* @get /residuals
+#* @get /fit/residuals
 function() {
   modelo <- lm(y ~ x + as.factor(grupo), data = df)
-  return(toJSON(modelo$residuals, pretty = TRUE))
+  return(modelo$residuals)
 }
+
 
 # Parte 3 - Eletiva
 #* Gera um gráfico dos resíduos do modelo de regressão ajustado
 #* @serializer png
-#* @get /plot_residuals
+#* @get /plot/residuals
 function() {
   modelo <- lm(y ~ x + as.factor(grupo), data = df)
   grafico <- df %>% ggplot(aes(sample = modelo$residuals)) +
     geom_qq() +
     theme_bw() +
-    labs(title = "Gráfico dos resíduos do modelo de regressão ajustado", 
-         subtitle = "Dados simulados", x = "Observação", y ="Resíduos")
+    labs(title = "Gráfico dos resíduos do modelo de regressão ajustado",
+         x = "Observação", y ="Resíduos")
   print(grafico) 
 }
 
 # Parte 3 - Eletiva
 #* Retorna informações sobre a significância estatística dos parâmetros
 #* @serializer json
-#* @get /stats_p-values
+#* @get /fit/p_values
 function() {
   modelo <- lm(y ~ x + as.factor(grupo), data = df)
   resultado <- list(
@@ -125,16 +146,18 @@ function() {
   return(resultado)
 }
 
+
 # Parte 4
 #* Predição para novas observações
 #* @param x Variável numérica
 #* @param grupo Variável categórica
 #* @serializer json
-#* @get /pred
+#* @parser json
+#* @get /fit/pred
 function(x, grupo) {
-  newdata <- data.frame(x=as.numeric(x), grupo)
+  x <- as.numeric(unlist(strsplit(x, ",")))
+  grupo <- unlist(strsplit(gsub(" ", "",grupo), ","))
+  newdata <- data.frame(x, grupo)
   pred <- predict(modelo, newdata)
   return(pred)
 }
-
-#
